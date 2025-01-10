7c2304d931b6cba17c434c60519cebde33e60aa4
AmalSultanov <pogboom1014@gmail.com>
2025-01-10 17:53:23 +0500

    added IngredientCategoryModel, IngredientModel and RecipeModel

--- ingredient_ai/recipes/admin.py
+++ ingredient_ai/recipes/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import IngredientCategoryModel, IngredientModel, RecipeModel
+
+
+@admin.register(IngredientCategoryModel)
+class IngredientCategoryModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['name']
+    ordering = ['name']
+    date_hierarchy = 'created_at'
+
+
+@admin.register(IngredientModel)
+class IngredientModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'created_at']
+    list_filter = ['category', 'created_at']
+    search_fields = ['name', 'category__name']
+
+
+@admin.register(RecipeModel)
+class RecipeModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'get_ingredients', 'created_at']
+    list_filter = ['cooking_time', 'serving_size', 'created_at']
+    search_fields = ['name', 'cooking_time', 'serving_size', 'description',
+                     'ingredients__name']
+
+    @staticmethod
+    def get_ingredients(obj):
+        return ', '.join(
+            [ingredient.name for ingredient in obj.ingredients.all()]
+        )
+
+    get_ingredients.short_description = 'Ingredients'
--- ingredient_ai/recipes/models.py
+++ ingredient_ai/recipes/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class IngredientCategoryModel(TimeStampedModel):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'ingredient category'
+        verbose_name_plural = 'ingredient categories'
+
+
+class IngredientModel(TimeStampedModel):
+    name = models.CharField(max_length=64)
+    category = models.ForeignKey(
+        IngredientCategoryModel,
+        on_delete=models.CASCADE,
+        related_name='ingredients'
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'ingredient'
+        verbose_name_plural = 'ingredients'
+
+
+class RecipeModel(TimeStampedModel):
+    image = models.ImageField(upload_to='recipes')
+    name = models.CharField(max_length=64)
+    cooking_time = models.PositiveSmallIntegerField()
+    serving_size = models.PositiveSmallIntegerField()
+    description = models.TextField()
+    ingredients = models.ManyToManyField(
+        IngredientModel,
+        related_name='recipes'
+    )
+    instructions = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'recipe'
+        verbose_name_plural = 'recipes'
