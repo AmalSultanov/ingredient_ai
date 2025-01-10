f81c8a473c1bb4de5ba9e940f0592b3529456538
AmalSultanov <pogboom1014@gmail.com>
2025-01-10 19:25:07 +0500

    added autocomplete_fields to RecipeModelAdmin

--- ingredient_ai/recipes/admin.py
+++ ingredient_ai/recipes/admin.py
@@ -17,6 +17,7 @@ class IngredientModelAdmin(admin.ModelAdmin):
     list_display = ['name', 'category', 'created_at']
     list_filter = ['category', 'created_at']
     search_fields = ['name', 'category__name']
+    ordering = ['category__name', 'name']
 
 
 @admin.register(RecipeModel)
@@ -25,6 +26,7 @@ class RecipeModelAdmin(admin.ModelAdmin):
     list_filter = ['cooking_time', 'serving_size', 'created_at']
     search_fields = ['name', 'cooking_time', 'serving_size', 'description',
                      'ingredients__name']
+    autocomplete_fields = ['ingredients']
 
     @staticmethod
     def get_ingredients(obj):
