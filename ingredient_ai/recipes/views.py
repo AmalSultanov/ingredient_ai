d9e9ba59840f4801aaa788449097272f4816a0de
AmalSultanov <pogboom1014@gmail.com>
2025-01-11 19:39:25 +0500

    added view for main url

--- ingredient_ai/recipes/views.py
+++ ingredient_ai/recipes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .selectors import get_categories_with_ingredients
+
+
+def get_ingredients(request):
+    categories_with_ingredients = get_categories_with_ingredients()
+    context = {'categories_with_ingredients': categories_with_ingredients}
+
+    return render(request, 'recipes/index.html', context)
