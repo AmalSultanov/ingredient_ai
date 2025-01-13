19cd2872616b7589fa30f839b9b29479e7330eaa
AmalSultanov <pogboom1014@gmail.com>
2025-01-13 20:46:18 +0500

    added get_prompt_file()

--- ingredient_ai/recipes/utils.py
+++ ingredient_ai/recipes/utils.py
@@ -3,6 +3,15 @@ import os
 from django.apps import apps
 
 
+def get_prompt_file():
+    prompt_file_path = get_prompt_file_path()
+
+    with open(prompt_file_path, 'r') as file:
+        prompt = file.read()
+
+    return prompt
+
+
 def get_prompt_file_path(filename='prompt.txt'):
     app_config = apps.get_app_config('recipes')
 
