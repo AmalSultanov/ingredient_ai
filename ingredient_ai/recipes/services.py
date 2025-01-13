5ed8de6cdf6da382709a3993afe28d2df4a06a6f
AmalSultanov <pogboom1014@gmail.com>
2025-01-13 23:57:13 +0500

    added recipes generation and saving functionalities

--- ingredient_ai/recipes/services.py
+++ ingredient_ai/recipes/services.py
@@ -1,8 +1,60 @@
+import json
+
+from ollama import chat
+
 from .models import RecipeModel
+from .utils import get_prompt_file
 
 
-def create_recipe(name, cooking_time, serving_size, description, ingredients,
-                  instructions):
+def generate_and_save_recipes(selected_ingredients):
+    prompt = get_prompt_file()
+    response = get_ai_response(prompt + ', '.join(selected_ingredients))
+
+    save_generated_recipes(response)
+
+
+def get_ai_response(prompt):
+    response = chat(
+        model='llama3.2:1b',
+        messages=[{
+            'role': 'user',
+            'content': prompt,
+        }]
+    )
+    content = response['message']['content']
+
+    return content
+
+
+def save_generated_recipes(response):
+    recipes_data = json.loads(response)
+
+    for recipe_data in recipes_data.get('recipes', []):
+        name = recipe_data.get('recipe_name', '')
+        cooking_time = recipe_data.get('cooking_time', '')
+        serving_size = recipe_data.get('serving_size', '')
+        description = recipe_data.get('description', '')
+        ingredients = '\n'.join(recipe_data.get('ingredients', []))
+        instructions = '\n'.join(recipe_data.get('instructions', []))
+
+        create_recipe(
+            name,
+            cooking_time,
+            serving_size,
+            description,
+            ingredients,
+            instructions
+        )
+
+
+def create_recipe(
+        name,
+        cooking_time,
+        serving_size,
+        description,
+        ingredients,
+        instructions
+):
     recipe = RecipeModel.objects.create(
         name=name,
         cooking_time=cooking_time,
