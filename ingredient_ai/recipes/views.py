f05273843cbb133673e69dc3cbf5170e32344f5b
AmalSultanov <pogboom1014@gmail.com>
2025-01-13 23:58:11 +0500

    added get_recipes() view

--- ingredient_ai/recipes/views.py
+++ ingredient_ai/recipes/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
-from .selectors import get_categories_with_ingredients
+from .selectors import (
+    get_categories_with_ingredients,
+    get_recipes_by_ingredients
+)
+from .services import generate_and_save_recipes
 
 
 def get_ingredients(request):
@@ -8,3 +12,14 @@ def get_ingredients(request):
     context = {'categories_with_ingredients': categories_with_ingredients}
 
     return render(request, 'recipes/index.html', context)
+
+
+def get_recipes(request):
+    if request.method == 'POST':
+        selected_ingredients = request.POST.getlist('ingredient')
+        generate_and_save_recipes(selected_ingredients)
+
+        recipes = get_recipes_by_ingredients(selected_ingredients)
+        context = {'recipes': recipes}
+
+        return render(request, 'recipes/recipes.html', context)
