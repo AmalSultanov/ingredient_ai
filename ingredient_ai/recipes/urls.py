75799da706bc6c90ad9505ac67f5de5c3fd553ce
AmalSultanov <pogboom1014@gmail.com>
2025-01-13 20:37:59 +0500

    removed generate-recipes url

--- ingredient_ai/recipes/urls.py
+++ ingredient_ai/recipes/urls.py
@@ -6,6 +6,5 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.get_ingredients, name='ingredients'),
-    path('generate-recipes/', views.generate_recipes, name='generate_recipes'),
     path('recipes/', views.get_recipes, name='recipes')
 ]
