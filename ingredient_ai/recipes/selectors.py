540c50840c6bc202b2214fcbb660367b7813d3ae
AmalSultanov <pogboom1014@gmail.com>
2025-01-13 12:33:33 +0500

    added get_recipes_by_ingredients()

--- ingredient_ai/recipes/selectors.py
+++ ingredient_ai/recipes/selectors.py
@@ -1,7 +1,18 @@
-from .models import IngredientCategoryModel
+from django.db.models import Q
+
+from .models import IngredientCategoryModel, RecipeModel
 
 
 def get_categories_with_ingredients():
     return IngredientCategoryModel.objects.prefetch_related(
         'ingredients'
     )
+
+
+def get_recipes_by_ingredients(selected_ingredients):
+    query = Q()
+
+    for ingredient in selected_ingredients:
+        query |= Q(ingredients__icontains=ingredient)
+
+    return RecipeModel.objects.filter(query).distinct()
