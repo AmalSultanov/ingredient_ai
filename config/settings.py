eafbade3cfc907d6c5821cef53e4f656b3b0172a
AmalSultanov <pogboom1014@gmail.com>
2025-01-14 19:56:47 +0500

    created 'users' app

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ingredient_ai.recipes'
+    'ingredient_ai.recipes',
+    'ingredient_ai.users'
 ]
 
 MIDDLEWARE = [
