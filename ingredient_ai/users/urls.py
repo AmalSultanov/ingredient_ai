74e2d2b174ac6a0e3b2c30c7aa9e0a54fcd35600
AmalSultanov <pogboom1014@gmail.com>
2025-01-14 20:25:54 +0500

    added urls for auth, custom registration and logout

--- ingredient_ai/users/urls.py
+++ ingredient_ai/users/urls.py
@@ -1,5 +1,11 @@
+from django.urls import path, include
+
+from . import views
+
 app_name = 'users'
 
 urlpatterns = [
-
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('register/', views.registration_view, name='register'),
+    path('logout', views.logout_view, name='logout'),
 ]
