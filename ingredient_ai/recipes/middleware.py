26e842e92bf9f0f5460b872686dac2d9d009fbd6
AmalSultanov <pogboom1014@gmail.com>
2025-01-15 21:04:41 +0500

    altered cached recipes deletion logic

--- ingredient_ai/recipes/middleware.py
+++ ingredient_ai/recipes/middleware.py
@@ -6,8 +6,20 @@ class ClearRecipeCacheMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        cache_key = f'recipes_{request.user.id}'
-        cache.delete(cache_key)
+        user_cache_key = f'user_recipes_key_{request.user.id}'
+
+        if request.path == '/recipes/':
+            selected_ingredients = request.POST.getlist('ingredient')
+            ingredients = '_'.join(selected_ingredients).lower()
+            cache_key = f'recipes_with_{ingredients}_by_{request.user.id}'
+
+            cache.set(user_cache_key, cache_key)
+        else:
+            cache_key = cache.get(user_cache_key)
+
+            if cache_key:
+                cache.delete(cache_key)
+                cache.delete(user_cache_key)
 
         response = self.get_response(request)
         return response
