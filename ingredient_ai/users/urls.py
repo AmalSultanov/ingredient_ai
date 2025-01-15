1916dee1efd7d34462e79c00f5b50cb3b527b199
AmalSultanov <pogboom1014@gmail.com>
2025-01-15 13:04:21 +0500

    changed url patterns for wishlist handling

--- ingredient_ai/users/urls.py
+++ ingredient_ai/users/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('registration/', views.registration_view, name='registration'),
+    path('add-to-wishlist/<int:pk>', views.add_to_wishlist,
+         name='add_to_wishlist'),
+    path('delete-from-wishlist/<int:pk>', views.delete_from_wishlist,
+         name='delete_from_wishlist'),
     path('wishlist/', views.get_wishlist_view, name='wishlist')
 ]
