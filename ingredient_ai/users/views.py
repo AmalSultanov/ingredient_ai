e05c12be154e6701dd8037b48e80e1244c9c4d3e
AmalSultanov <pogboom1014@gmail.com>
2025-01-20 00:40:46 +0500

    added custom login CBV and registration FBV

--- ingredient_ai/users/views.py
+++ ingredient_ai/users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import RegistrationForm
+from .utils import add_to_wishlist, delete_from_wishlist, get_wishlist
+
+
+class CustomLoginView(LoginView):
+    template_name = 'users/registration/login.html'
+
+
+def registration_view(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+
+            return redirect('/')
+
+    form = RegistrationForm()
+    context = {'form': form}
+
+    return render(request, 'users/registration/registration.html', context)
