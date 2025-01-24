e0c43eb97b23749a2c05de97564ea79cdbc62608
AmalSultanov <pogboom1014@gmail.com>
2025-01-24 14:10:36 +0500

    added AWS S3 for static and media files

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ingredient_ai.recipes',
     'ingredient_ai.users',
-    'debug_toolbar'
+    'debug_toolbar',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -118,5 +119,27 @@ CACHES = {
         'TIMEOUT': None
     }
 }
+
+STORAGES = {
+    'default': {
+        'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
+        'OPTIONS': {
+            'location': 'media',
+        },
+    },
+    'staticfiles': {
+        'BACKEND': 'storages.backends.s3boto3.S3StaticStorage',
+        'OPTIONS': {
+            'location': 'static',
+        },
+    },
+}
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_FILE_OVERWRITE = False
+AWS_LOCATION = 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
